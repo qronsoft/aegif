964390e43350cf6dfd43cae3a677ed30bada5157
qronsoft <qron.soft@gmail.com>
2020-12-29 16:57:55 +0900

    fix exception handling

--- aegif/src/aegif.cpp
+++ aegif/src/aegif.cpp
@@ -13,35 +13,41 @@
 
 namespace
 {
-#define CATCH_EXCEPTION                                                                                      \
-    catch (const std::exception& ex)                                                                         \
-    {                                                                                                        \
-        AEGLOG_ERROR("catch exception: {}", aegif::OEMToU8(ex.what()));                                      \
-        return A_Err_GENERIC;                                                                                \
-    }                                                                                                        \
-    catch (A_Err err) { return err; }                                                                        \
-    catch (...)                                                                                              \
-    {                                                                                                        \
-        AEGLOG_ERROR("catch unknown exception");                                                             \
-        return A_Err_GENERIC;                                                                                \
-    }
+template <void* Fn, class... Args>
+A_Err safe_callback(Args... args) try
+{
+    return Fn(args...);
+}
+catch (const std::exception& ex)
+{
+    AEGLOG_ERROR("catch exception: {}", aegif::OEMToU8(ex.what()));
+    return A_Err_GENERIC;
+}
+catch (A_Err err)
+{
+    AEGLOG_ERROR("catch A_Err");
+    return err;
+}
+catch (...)
+{
+    AEGLOG_ERROR("catch unknown exception");
+    return A_Err_GENERIC;
+}
 
-A_Err DeathHook(AEGP_GlobalRefcon global_refcon, AEGP_DeathRefcon death_refcon) try
+A_Err DeathHook(AEGP_GlobalRefcon global_refcon, AEGP_DeathRefcon death_refcon)
 {
     AEGLOG_TRACE("DeathHook called");
 
     aegif::SetdownLog();
     return A_Err_NONE;
 }
-CATCH_EXCEPTION
-}
 
-extern "C" DllExport A_Err EntryPointFunc(
+A_Err GlobalSetup(
     struct SPBasicSuite* pica_basicP,
     A_long major_versionL,
     A_long minor_versionL,
     AEGP_PluginID aegp_pluginId,
-    AEGP_GlobalRefcon* global_refconP) try
+    AEGP_GlobalRefcon* global_refconP)
 {
     aegif::SetupLog(pica_basicP);
 
@@ -50,7 +56,7 @@ extern "C" DllExport A_Err EntryPointFunc(
     AEGP_SuiteHandler suites(pica_basicP);
 
     AEGLOG_TRACE("register death hook");
-    suites.RegisterSuite5()->AEGP_RegisterDeathHook(aegp_pluginId, DeathHook, nullptr);
+    suites.RegisterSuite5()->AEGP_RegisterDeathHook(aegp_pluginId, safe_callback<DeathHook>, nullptr);
 
     GifskiSettings gifskiSettings = {};
     gifski* gifski                = gifski_new(&gifskiSettings);
@@ -68,4 +74,15 @@ extern "C" DllExport A_Err EntryPointFunc(
 
     return A_Err_NONE;
 }
-CATCH_EXCEPTION
+}
+
+extern "C" DllExport A_Err EntryPointFunc(
+    struct SPBasicSuite* pica_basicP,
+    A_long major_versionL,
+    A_long minor_versionL,
+    AEGP_PluginID aegp_pluginId,
+    AEGP_GlobalRefcon* global_refconP)
+{
+    return safe_callback<GlobalSetup>(
+        pica_basicP, major_versionL, minor_versionL, aegp_pluginId, global_refconP);
+}
