19d6a34c3c4baff408795a24ee5eeb5d3a166319
qronsoft <qron.soft@gmail.com>
2021-01-09 22:28:51 +0900

    bump version 0.0.1rc0

--- aegif/src/version.hpp
+++ aegif/src/version.hpp
@@ -3,5 +3,5 @@
 #define AEGIF_MAJOR_VERSION 0 /* 0-7 */
 #define AEGIF_MINOR_VERSION 0 /* 0-15 */
 #define AEGIF_BUG_VERSION 1   /* 0-15 */
-#define AEGIF_STAGE_VERSION 2 /* develop: 0, alpha: 1, beta: 2, rc: 3 */
+#define AEGIF_STAGE_VERSION 3 /* develop: 0, alpha: 1, beta: 2, rc: 3 */
 #define AEGIF_BUILD_VERSION 0 /* 0-511 */
